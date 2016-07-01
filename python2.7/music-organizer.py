71905983d6d32cf82b6068cc8c6cad693e0094c4
joostsijm <joostsijm@gmail.com>
2016-07-01 16:19:05 +0200

    Flags now have abbreviations

--- python2.7/music-organizer.py
+++ python2.7/music-organizer.py
@@ -30,35 +30,35 @@ parser = argparse.ArgumentParser(
     All names are made lowercase and separated by dashes for easier
     navigation in a Linux filesystem.'''
     )
-parser.add_argument('--delete-conflicts', action='store_true',
+parser.add_argument('-d','--delete-conflicts', action='store_true',
         dest='delete_conflicts',
         help='''If an artist has duplicate tracks with the same name,
                     delete them. Note this might always be best in case an
                     artist has multiple versions. To keep multiple versions,
                     fix the tag information.''')
-parser.add_argument('--ignore-multiple-artists', action='store_true',
+parser.add_argument('-m','--ignore-multiple-artists', action='store_true',
         dest='ignore_multiple_artists',
         help='''This script will prompt for confirmation if an artist
                     directory has songs with more than 2 different tags.
                     This flag disables the confirmation and won't perform
                     this check.''')
-parser.add_argument('--collection', action='store_true',
+parser.add_argument('-c','--collection', action='store_true',
         help='''Operate in 'collection' mode and run 'artist' mode
                     on every subdirectory.''')
-parser.add_argument('--artist', action='store_true',
+parser.add_argument('-a', '--artist', action='store_true',
         help='''Operate in 'artist' mode and copy all songs to the
                     root of the directory and cleanly format the names to
                     be easily typed and navigated in a shell.''')
-parser.add_argument('--delete-unrecognized-extensions', action='store_true',
+parser.add_argument('-e','--delete-unrecognized-extensions', action='store_true',
         dest='delete_unrecognized')
-parser.add_argument('--album', action='store_true',
+parser.add_argument('-A','--album', action='store_true',
         dest='album',
         help='''Adds album folder inside the artist folder to sort out
                     albums''')
-parser.add_argument('--numbering', action='store_true',
+parser.add_argument('-n','--numbering', action='store_true',
         dest='numbering',
         help='''Adds numbering in front of sorted songs''')
-parser.add_argument('--capital', action='store_true',
+parser.add_argument('-C','--capital', action='store_true',
         dest='capital',
         help='''Makes the first letter of a song capital''')
 args = parser.parse_args()
