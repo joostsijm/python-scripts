c54d5741355d13a66eaef45f4a798ec1558cbcfc
joostsijm <joostsijm@gmail.com>
2016-07-04 16:28:04 +0200

    updating music tag fixer and adding music number fixer

--- python2.7/fix-music-tags.py
+++ python2.7/fix-music-tags.py
@@ -8,8 +8,10 @@ This script (fix-music-tags.py) mass-removes unwanted music tags.
 """
 
 from mutagen.easyid3 import EasyID3
+import os
 import argparse
 import glob
+import re
 
 
 def fixTags(fname, keep):
@@ -24,14 +26,32 @@ def fixTags(fname, keep):
         del audio[k]
     audio.save()
 
+def fixNumber(fname):
+    audio = EasyID3(fname)
+
+    if `'tracknumber'` in audio:
+        return
+
+    else:
+        try:
+            tracknumber = re.findall(r'\d+', os.path.basename(fname).split(' ')[0])[0]
+            audio['tracknumber'] = tracknumber.zfill(2) 
+            audio.save()
+        except:
+            return
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('directory', help='Directory with mp3 files to fix.')
     parser.add_argument('--keep', default=['title', 'artist', 'album', 'genre'],
-                        type=str, nargs='+', metavar='TAG',
-                        help="Tags to keep. Default: title, artist, album, genre")
+            type=str, nargs='+', metavar='TAG',
+            help="Tags to keep. Default: title, artist, album, genre")
+    parser.add_argument('--fixnumber', action='store_true',
+            help="Trying to fix song number.")
     args = parser.parse_args()
 
     for fname in glob.glob("{}/*.mp3".format(args.directory)):
         print("Fixing tags for {}".format(fname))
         fixTags(fname, args.keep)
+        if args.fixnumber:
+            fixNumber(fname)
